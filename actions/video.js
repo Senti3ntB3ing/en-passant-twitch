0f03dc7d3740c47056d106d8c0c068497f618f10
Cristian-A <cristianantonuccio@icloud.com>
2022-07-29 00:22:20 +0200

    fix

--- actions/video.js
+++ actions/video.js
@@ -6,6 +6,6 @@ programmable({
 	commands: [ 'video' ],
 	description: 'Gets Zach\'s current video link.',
 	execute: async () => 'Check out Zach\'s new YouTube video: ' +
-		(await Database.get('yt_video_id')) + ' -> ' +
+		(await Database.get('yt_video_title')) + ' -> ' +
 		(await Database.get('yt_video_link'))
 });
