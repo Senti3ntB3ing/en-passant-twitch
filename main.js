1cf3f124ff420f2ea1412c796865a54ed022b2af
Cristian-A <cristianantonuccio@icloud.com>
2022-11-17 21:15:58 +0100

    drop ended

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ import './actions/video.js';
 // ==== Tasks ==============================
 
 import './tasks/discord.js';
-import './tasks/drop.js';
+// import './tasks/drop.js';
 
 // =========================================
 
