4cdd16f7c516f7be896a0930d484b50e6c39f167
Cristian-A <cristianantonuccio@icloud.com>
2023-06-27 11:59:42 +0200

    aliases for video

--- actions/video.js
+++ actions/video.js
@@ -3,7 +3,7 @@ import { programmable } from "../parser.js";
 import { Database } from "../database.js";
 
 programmable({
-	commands: [ "video" ],
+	commands: [ "video", "newvid", "newvideo" ],
 	description: "Gets Zach's current video link.",
 	execute: async () => "Check out Zach's new YouTube video: " +
 		(await Database.get("yt_video_title")) + " -> " +
