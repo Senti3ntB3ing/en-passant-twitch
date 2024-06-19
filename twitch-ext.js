35997a2f066db5922877b3ccf250caa200016976
Chance <cloricco13@icloud.com>
2024-06-19 12:12:01 -0400

    jwt fixes

--- twitch-ext.js
+++ twitch-ext.js
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+import { validateJwt } from "https://deno.land/x/djwt/validate.ts";
 
 import { Database } from "./database";
 
@@ -7,16 +7,26 @@ import { Database } from "./database";
 
 const key = await Database.get("twitch_ext_secret");
 const secret = Buffer.from(key, 'base64');
+const bearerPrefix = 'Bearer '; 
 
-export function verifyAndDecode (header) {
+export async function verifyAndDecode (header) {
     console.log("in verify")
     if (header.startsWith(bearerPrefix)) {
       try {
         const token = header.substring(bearerPrefix.length);
-        return jwt.verify(token, secret, { algorithms: ['HS256'] });
+        return (await validateJwt({ jwt, key, algorithm: "HS256" })).isValid;
       }
       catch (ex) {
         return console.log("Invalid JWT");
       }
     }
 }
+
+// export function extMiddle(req, res, next) {
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+//     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST');
+//     // Note that the origin of an extension iframe will be null
+//     // so the Access-Control-Allow-Origin has to be wildcard.
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     next();
+// };
